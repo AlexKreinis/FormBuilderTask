99c17856d643f8238cfe6eaa8bea6b00fa17a017
Alex <alex.kreinis@walla.co.il>
2020-03-05 02:12:33 +0200

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 connectDB();
 
-app.get("/", (req, res) => res.send("API RUNNING"));
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ extended: false }));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.use("/api/form", require("./routes/api/FormAPI"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
